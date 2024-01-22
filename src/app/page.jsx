0a6cfc427b657fcec5a84fb489644f4b767de583
Ayegba John <ayegbajohn@gmail.com>
2024-01-22 13:52:18 +0100

    changed the UI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,16 @@
 'use client'
 
 import { useRef, useState } from "react"
-import Navbar from "./Navbar";
-import Home from "./Home"
 
 const page = ()=>{
-  const title = 'This is great to know';
-  const like = 50;
-  const link = 'https://www.google.com';
-  
-  return(<div className="App">
-    <div>
-      <h1 className="Com">Componets</h1>
-      <Navbar />
-      <h2>{title}</h2>
-      <h4>liked {like} times</h4>
-      <Home />
-    </div>
 
+  const [data,setData] = useState([]);
+  const inputRef = useRef(null);
+  return(
+  <div>
+    <input ref={inputRef} type="text"/>
+    <button onClick={()=>{setData([data,inputRef.current.value])}}>Submit</button>
+    {data.map((item,index)=>{return <h2 key={index}>{item}</h2>})}
   </div>)
 }
 export default page
-
-
-
-
